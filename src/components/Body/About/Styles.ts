43ff548ca03501c03f6319d4781f3c09f668dc0b
Ayush Mishra <ayushm017@gmail.com>
2020-08-21 18:22:11 +0530

    added about page styles

--- src/components/Body/About/Styles.ts
+++ src/components/Body/About/Styles.ts
@@ -1,6 +1,31 @@
 import styled from "styled-components";
 
 export const AboutContainer = styled.div`
-	display: flex;
-	flex-wrap: wrap;
+	width: 100vw;
+	margin: 0 2rem;
+`;
+
+export const AboutBlock = styled.div`
+	margin: 1rem;
+`;
+
+export const List = styled.ul`
+	margin: 0;
+`;
+
+export const ListItem = styled.li`
+	margin: 0;
+	&:before {
+		content: "";
+	}
+`;
+
+export const Description = styled.div`
+	display: inline-block;
+	margin: 0 1rem;
+	padding: 0;
+
+	& > * {
+		margin: 0 auto;
+	}
 `;
